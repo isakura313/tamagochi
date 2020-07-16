47353715558dd88f046d34dd61cbcce49fa52809
Paul <isakura313@gmail.com>
2020-07-16 18:56:48 +0300

    asdasd

--- tamagochi.py
+++ tamagochi.py
@@ -4,7 +4,12 @@ from tkinter import ttk
 def game():
     display = tk.Tk()
     display.geometry("400x400")
-    display.mainloop()
 
+    btn1 = ttk.Button(display, text = "Начать новую игру")
+    btn1.place(x = 70, y =120)
+
+    btn2 = ttk.Button(display, text = "Продолжить игру")
+    btn2.place(relx = 0.25, rely = 0.5)
+    display.mainloop()
 
 game()
